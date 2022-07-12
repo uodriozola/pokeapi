9a6a358321fa63672278381fd5963c1d4d176479
Unai Odriozola <unai.odriozola@crealogix.com>
2022-07-12 16:49:22 +0200

    First commit

---
+++ src/main.ts
@@ -0,0 +1,19 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  app.enableCors();
+
+  const host = process.env.host || 'localhost';
+  const port = process.env.PORT || 3001;
+
+  if (process.env.DOCKER) {
+    await app.listen(port);
+  } else {
+    await app.listen(port, host);
+  }
+}
+
+bootstrap();
